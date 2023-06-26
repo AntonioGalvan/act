3f3f9aaaa013568131d600a4f7d121f6acdba846
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2023-06-25 21:30:07 -0600

    ProjectModel
    Se añadio paquete nugget Microsoft.EntityFrameworkCore.Sqlite versión: 7.0.8

---
+++ act/Models/ProjectModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace act.Models
+{
+    internal class ProjectModel
+    {
+        private int id;
+        private string name;
+        private string description;
+        private DateOnly startDate;
+        private DateOnly endDate;
+
+        [DisplayName("Id")]
+        [Required(ErrorMessage = "Id es requerido")]
+        public int Id { get { return id; } set {  id = value; } }
+
+        [DisplayName("Nombre")]
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Name { get { return name; } set { name = value; } }
+
+        [DisplayName("Descripción")]
+        [Required(ErrorMessage = "La descripción es requerida")]
+        public string Description { get { return description; } set { description = value; } }
+
+        [DisplayName("Fecha de inicio")]
+        [Required(ErrorMessage = "La fecha de inicio es requerida")]
+        public DateOnly StartDate { get {  return startDate; } set { startDate = value; } }
+
+        [DisplayName("Fecha de finalización")]
+        [Required(ErrorMessage = "La fecha de finalización es requerida")]
+        public DateOnly EndDate { get {  return endDate; } set {  endDate = value; } }
+    }
+}
