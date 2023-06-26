b65605df9f47cb3cb70d2597add42c1fe99ac3db
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2023-06-25 21:33:13 -0600

    ProjectRepository

---
+++ act/Models/IProjectRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace act.Models
+{
+    internal interface IProjectRepository
+    {
+        void Add(ProjectModel projectModel);
+        void Edit(ProjectModel projectModel);
+        void Delete(int id);
+
+        //Listados
+        IEnumerable<ProjectModel> GetAll();
+
+        //Busquedas
+        IEnumerable<ProjectModel> GetByValue();
+    }
+}
